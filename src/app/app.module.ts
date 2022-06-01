581f8273136274b7a594d4d23911801aca09ce32
maydragon2000 <may-dragon2000@outlook.com>
2022-06-01 08:40:39 +0800

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+@NgModule({
+
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, HomeComponent]
+})
+export class AppModule { }
